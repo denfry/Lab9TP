06a450d62e1f39823b056c2c942a02a7b84df607
PavelK <dabinayo@pm.me>
2024-03-28 12:30:16 +0300

    Добавьте файлы проекта.

---
+++ LabTP9/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LabTP9
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPaint = new System.Windows.Forms.Button();
+            this.checkBoxWalls = new System.Windows.Forms.CheckBox();
+            this.checkBoxWindows = new System.Windows.Forms.CheckBox();
+            this.checkBoxRoof = new System.Windows.Forms.CheckBox();
+            this.checkBoxDoor = new System.Windows.Forms.CheckBox();
+            this.checkBoxPipe = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // buttonPaint
+            // 
+            this.buttonPaint.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonPaint.Location = new System.Drawing.Point(29, 358);
+            this.buttonPaint.Name = "buttonPaint";
+            this.buttonPaint.Size = new System.Drawing.Size(95, 39);
+            this.buttonPaint.TabIndex = 0;
+            this.buttonPaint.Text = "Рисовать";
+            this.buttonPaint.UseVisualStyleBackColor = true;
+            this.buttonPaint.Click += new System.EventHandler(this.buttonPaint_Click);
+            // 
+            // checkBoxWalls
+            // 
+            this.checkBoxWalls.AutoSize = true;
+            this.checkBoxWalls.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxWalls.Location = new System.Drawing.Point(29, 30);
+            this.checkBoxWalls.Name = "checkBoxWalls";
+            this.checkBoxWalls.Size = new System.Drawing.Size(69, 21);
+            this.checkBoxWalls.TabIndex = 1;
+            this.checkBoxWalls.Text = "Стены";
+            this.checkBoxWalls.UseVisualStyleBackColor = true;
+            this.checkBoxWalls.CheckedChanged += new System.EventHandler(this.checkBoxWalls_CheckedChanged);
+            // 
+            // checkBoxWindows
+            // 
+            this.checkBoxWindows.AutoSize = true;
+            this.checkBoxWindows.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxWindows.Location = new System.Drawing.Point(31, 90);
+            this.checkBoxWindows.Name = "checkBoxWindows";
+            this.checkBoxWindows.Size = new System.Drawing.Size(61, 21);
+            this.checkBoxWindows.TabIndex = 2;
+            this.checkBoxWindows.Text = "Окна";
+            this.checkBoxWindows.UseVisualStyleBackColor = true;
+            this.checkBoxWindows.CheckedChanged += new System.EventHandler(this.checkBoxWindows_CheckedChanged);
+            // 
+            // checkBoxRoof
+            // 
+            this.checkBoxRoof.AutoSize = true;
+            this.checkBoxRoof.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxRoof.Location = new System.Drawing.Point(29, 159);
+            this.checkBoxRoof.Name = "checkBoxRoof";
+            this.checkBoxRoof.Size = new System.Drawing.Size(73, 21);
+            this.checkBoxRoof.TabIndex = 3;
+            this.checkBoxRoof.Text = "Крыша";
+            this.checkBoxRoof.UseVisualStyleBackColor = true;
+            this.checkBoxRoof.CheckedChanged += new System.EventHandler(this.checkBoxRoof_CheckedChanged);
+            // 
+            // checkBoxDoor
+            // 
+            this.checkBoxDoor.AutoSize = true;
+            this.checkBoxDoor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxDoor.Location = new System.Drawing.Point(29, 225);
+            this.checkBoxDoor.Name = "checkBoxDoor";
+            this.checkBoxDoor.Size = new System.Drawing.Size(68, 21);
+            this.checkBoxDoor.TabIndex = 4;
+            this.checkBoxDoor.Text = "Дверь";
+            this.checkBoxDoor.UseVisualStyleBackColor = true;
+            this.checkBoxDoor.CheckedChanged += new System.EventHandler(this.checkBoxDoor_CheckedChanged);
+            // 
+            // checkBoxPipe
+            // 
+            this.checkBoxPipe.AutoSize = true;
+            this.checkBoxPipe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxPipe.Location = new System.Drawing.Point(29, 292);
+            this.checkBoxPipe.Name = "checkBoxPipe";
+            this.checkBoxPipe.Size = new System.Drawing.Size(67, 21);
+            this.checkBoxPipe.TabIndex = 5;
+            this.checkBoxPipe.Text = "Труба";
+            this.checkBoxPipe.UseVisualStyleBackColor = true;
+            this.checkBoxPipe.CheckedChanged += new System.EventHandler(this.checkBoxPipe_CheckedChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.progressBar1.Location = new System.Drawing.Point(222, 1);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(865, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(222, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(865, 626);
+            this.panel1.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(1099, 668);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBoxPipe);
+            this.Controls.Add(this.checkBoxDoor);
+            this.Controls.Add(this.checkBoxRoof);
+            this.Controls.Add(this.checkBoxWindows);
+            this.Controls.Add(this.checkBoxWalls);
+            this.Controls.Add(this.buttonPaint);
+            this.Name = "MainForm";
+            this.Text = "Лабораторная работа №9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPaint;
+        private System.Windows.Forms.CheckBox checkBoxWalls;
+        private System.Windows.Forms.CheckBox checkBoxWindows;
+        private System.Windows.Forms.CheckBox checkBoxRoof;
+        private System.Windows.Forms.CheckBox checkBoxDoor;
+        private System.Windows.Forms.CheckBox checkBoxPipe;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ LabTP9/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabTP9
+{
+    public partial class MainForm : Form
+    {
+        public delegate void DrawDelegate(Graphics graphics);
+        private DrawDelegate drawDelegate;
+        public MainForm()
+        {
+            InitializeComponent();
+            drawDelegate = g =>
+            {
+                // Рисование неба и солнца
+                g.FillRectangle(Brushes.LightBlue, panel1.ClientRectangle); // Рисуем небо
+                g.FillEllipse(Brushes.Yellow, new Rectangle(250, 30, 50, 50)); // Рисуем солнце
+            };
+        }
+
+        
+        // Статический метод
+        private static void DrawWallsStatic(Graphics graphics)
+        {
+            graphics.FillRectangle(Brushes.Gray, new Rectangle(50, 50, 865, 626));
+        }
+        // Нестатический метод
+        private void DrawWindowsNonStatic(Graphics graphics)
+        {
+            graphics.FillRectangle(Brushes.White, new Rectangle(300, 200, 100, 100));
+            graphics.FillRectangle(Brushes.White, new Rectangle(600, 200, 100, 100));
+        }
+    
+        // Статический метод пользовательского класса
+        private static void DrawRoofStatic(Graphics graphics)
+        {
+            DrawRoofAndDoor.DrawRoofStatic(graphics);
+        }
+        // Нестатический метод пользовательского класса
+        private void DrawDoorNonStatic(Graphics graphics)
+        {
+            var drawDoor = new DrawRoofAndDoor();
+            drawDoor.DrawDoorNonStatic(graphics);
+        }
+        // Нестатический метод, определяемый лямбда-выражением
+        private static DrawDelegate DrawPipe = g =>
+        {
+            g.FillRectangle(Brushes.Gray, new Rectangle(600, 100, 100, 150));
+        };
+        private void buttonPaint_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(panel1.BackColor);
+            drawDelegate.Invoke(g);
+        }
+        class DrawRoofAndDoor
+        {
+            public static void DrawRoofStatic(Graphics graphics)
+            {
+                Point[] roofPoints =
+                {
+                    new Point(222, 30),
+                    new Point(666, 230),
+                    new Point(1110, 30)
+                };
+                graphics.FillPolygon(Brushes.Red, roofPoints);
+            }
+            public void DrawDoorNonStatic(Graphics graphics)
+            {
+                graphics.FillRectangle(Brushes.Brown, new Rectangle(450, 400, 200, 300));
+            }
+        }
+
+        private void checkBoxWalls_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxWalls.Checked)
+            {
+                drawDelegate += DrawWallsStatic;
+            }
+            else
+            {
+                drawDelegate -= DrawWallsStatic;
+            }
+            UpdateProgressBarValue();
+        }
+
+        private void checkBoxWindows_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxWindows.Checked)
+            {
+                drawDelegate += DrawWindowsNonStatic;
+            }
+            else
+            {
+                drawDelegate -= DrawWindowsNonStatic;
+            }
+            UpdateProgressBarValue();
+        }
+
+        private void checkBoxRoof_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxRoof.Checked)
+            {
+                drawDelegate += DrawRoofStatic;
+            }
+            else
+            {
+                drawDelegate -= DrawRoofStatic;
+            }
+            UpdateProgressBarValue();
+        }
+
+        private void checkBoxDoor_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxDoor.Checked)
+            {
+                drawDelegate += DrawDoorNonStatic;
+            }
+            else
+            {
+                drawDelegate -= DrawDoorNonStatic;
+            }
+            UpdateProgressBarValue();
+        }
+
+        private void checkBoxPipe_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxPipe.Checked)
+            {
+                drawDelegate += DrawPipe;
+            }
+            else
+            {
+                drawDelegate -= DrawPipe;
+            }
+            UpdateProgressBarValue();
+        }
+        private void UpdateProgressBarValue()
+        {
+            int checkedCount = 0;
+            if (checkBoxWalls.Checked) checkedCount++;
+            if (checkBoxWindows.Checked) checkedCount++;
+            if (checkBoxRoof.Checked) checkedCount++;
+            if (checkBoxDoor.Checked) checkedCount++;
+            if (checkBoxPipe.Checked) checkedCount++;
+
+            progressBar1.Value = (int)(((double)checkedCount / 5) * 100); // 5 - количество чекбоксов
+        }
+    }
+}
